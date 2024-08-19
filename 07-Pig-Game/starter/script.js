eb6a4822b23b165661ecec65a347181a26b6a277
Jesse Smart <jsmartdev@protonmail.com>
2024-08-19 17:06:22 -0400

    dice roll functional

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const score0 = document.getElementById('score--0');
+const score1 = document.getElementById('score--1');
+const dice = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+//starting conditions
+score0.textContent = '0';
+score1.textContent = '0';
+dice.classList.add('hidden');
+
+btnRoll.addEventListener('click', () => {
+  const roll = Math.trunc(Math.random() * 6) + 1;
+  dice.classList.remove('hidden');
+  dice.src = `dice-${roll}.png`;
+})
