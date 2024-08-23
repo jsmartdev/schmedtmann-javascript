8ac324625f10b766541d630bc25baebd50ca76f5
Jesse Smart <jsmartdev@protonmail.com>
2024-08-23 16:17:17 -0400

    scoping in practice part 1

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const calcAge = (birthYear) => {
+  const age = 2037 - birthYear;
+  console.log(`${firstName} is ${age}`);
+  return age;
+}
+
+const firstName = 'Jonas';
+
+calcAge(1991);
